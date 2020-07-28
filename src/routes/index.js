2b9730e3fa1bc2071b6adaa220c6e79231b07e91
Matheus-fdte <matheusbarbieri@MacBook-Pro.local>
2020-07-28 15:37:32 -0300

    adding workflow cockpit routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 const Router = require('koa-router');
+// rutes
 const healthCheckRouter = require('./health-check');
+const workflowRouter = require('./workflow');
+// middlewares
 const { captureActorData } = require('../middlewares');
 
 module.exports = () => {
   const hcRouter = healthCheckRouter();
-
+  const wRouter = workflowRouter();
   const mainRouter = new Router();
   mainRouter.use(captureActorData);
   mainRouter.use(hcRouter.middleware());
+  mainRouter.use(wRouter.middleware());
   return mainRouter;
 };
